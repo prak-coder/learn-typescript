cd76edbd58e35aafc5fa200c519070c24344b0f8
Prakash K <prakash.change@gmail.com>
2026-05-07 16:35:42 +0530

    types implict type and any type

---
+++ variables/index.ts
@@ -0,0 +1,9 @@
+const a: number = 21;
+console.log(a);
+const userName: string = "troy";
+console.log(userName);
+let isLoading: boolean = true;
+isLoading = "n";
+// implicit typesx
+let variableName = "x";
+let state;
