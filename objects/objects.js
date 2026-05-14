0c1408c5e136b0b0d8763426c2f2b07d9154feb5
Prakash K <prakash.change@gmail.com>
2026-05-14 17:27:31 +0530

    type to create a newshape of object Point

--- objects/objects.js
+++ objects/objects.js
@@ -1,7 +1,11 @@
-function printName(person) {
-    console.log(person);
+// function printName(person: { first: string; last: string }): void {
+//   console.log(person);
+// }
+function calcAddTwo(point) {
+    return {
+        x: point.x + 2,
+        y: point.y + 2,
+    };
 }
-var user = { first: "tom", last: "hanks" };
-var halfUser = { first: "jimmy" };
-printName(user);
-printName(halfUser);
+var p = { x: 5, y: 9 };
+console.log(calcAddTwo(p));
--- objects/objects.ts
+++ objects/objects.ts
@@ -1,8 +1,23 @@
-function printName(person: { first: string; last: string }): void {
-  console.log(person);
-}
+// function printName(person: { first: string; last: string }): void {
+//   console.log(person);
+// }
 
-const user = { first: "tom", last: "hanks" };
-const halfUser = { first: "jimmy" };
-printName(user);
-printName(halfUser);
+// const user = { first: "tom", last: "hanks" };
+// const halfUser = { first: "jimmy" };
+// printName(user);
+// printName(halfUser);
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+function calcAddTwo(point: Point): Point {
+  return {
+    x: point.x + 2,
+    y: point.y + 2,
+  };
+}
+const p: Point = { x: 5, y: 9 };
+
+console.log(calcAddTwo(p));
