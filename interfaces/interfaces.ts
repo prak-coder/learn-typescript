b01f3e9a270f3084b0e1105986133af0ea789c4d
prak-coder <prakash.change@gmail.com>
2026-09-26 10:53:48 +0530

    ts revision

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,49 +1,53 @@
 interface Person {
+  readonly id: number;
   name: string;
   age: number;
   sayHi(): string;
+  nickname?: string;
 }
 
 const user: Person = {
+  id: 222,
   name: "troy",
   age: 18,
   sayHi: () => "Hi",
+  nickname: "t-bone",
 };
-console.log(user.name);
-console.log(user.sayHi());
+// console.log(user.name);
+// console.log(user.sayHi());
 
-interface Dog {
-  name: string;
-  age: number;
-}
+// interface Dog {
+//   name: string;
+//   age: number;
+// }
 
-interface Dog {
-  breed: string;
-  bark: () => string;
-}
+// interface Dog {
+//   breed: string;
+//   bark: () => string;
+// }
 
-const myDog: Dog = {
-  name: "browny",
-  age: 5,
-  breed: "indog",
-  bark: () => "Woof Woof",
-};
-console.log(myDog);
+// const myDog: Dog = {
+//   name: "browny",
+//   age: 5,
+//   breed: "indog",
+//   bark: () => "Woof Woof",
+// };
+// console.log(myDog);
 
-interface ServiceDog extends Dog {
-  name: string;
-  age: number;
-  breed: string;
-  bark: () => string;
-  job: "drug dog" | "ptsd dog" | "bomb dog";
-}
+// interface ServiceDog extends Dog {
+//   name: string;
+//   age: number;
+//   breed: string;
+//   bark: () => string;
+//   job: "drug dog" | "ptsd dog" | "bomb dog";
+// }
 
-const dynamite: ServiceDog = {
-  name: "dynamite",
-  age: 6,
-  breed: "bloodhound",
-  bark: () => "Bark",
-  job: "bomb dog",
-};
-console.log(dynamite.job);
+// const dynamite: ServiceDog = {
+//   name: "dynamite",
+//   age: 6,
+//   breed: "bloodhound",
+//   bark: () => "Bark",
+//   job: "bomb dog",
+// };
+// console.log(dynamite.job);
 //type vs interfaces interface can extend can be re declared only object types can be redeclared add poperties after declaration
